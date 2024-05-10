579ea87ca530d0a7cd0ea9ab003889f9ffff836e
satyamkumarjha1 <satyamkumarjha243@gmail.com>
2024-05-10 10:00:06 +0530

    1st commit

--- lib/user/userView.dart
+++ lib/user/userView.dart
@@ -9,33 +9,48 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Obx(
-      () {
-        if (userController.isLoading.value) {
-          print('api called');
-          return Center(child: CircularProgressIndicator());
-        }
-
-        return ListView.builder(
-          itemCount: userController.userList.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: (Container(
-                color: Colors.blue,
-                child: ListTile(
-                  trailing: Text(
-                      userController.userList[index].company.bs.toString()),
-                  title: Text(userController.userList[index].name),
-                  subtitle: Text(
-                    userController.userList[index].address.geo.lat.toString(),
+    return Scaffold(
+      body: Obx(
+        () {
+          if (userController.isLoading.value) {
+            print('api called');
+            return const Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+            itemCount: userController.userList.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: (Container(
+                  color: Colors.blue.shade100,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          color: Colors.white24,
+                          child: Text(
+                              'UserName : ${userController.userList[index].username}'),
+                        ),
+                        Text(
+                            'CompanyBS: ${userController.userList[index].company.bs.toString()}'),
+                        Text('Name : ${userController.userList[index].name}'),
+                        Text(
+                          'Phone : ${userController.userList[index].phone.toString()}',
+                        ),
+                        Text(
+                            'Website : ${userController.userList[index].website.toString()}')
+                      ],
+                    ),
                   ),
-                ),
-              )),
-            );
-          },
-        );
-      },
-    ));
+                )),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
